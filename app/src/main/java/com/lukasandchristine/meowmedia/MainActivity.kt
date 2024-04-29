85236eb5b005da3f685c2e09427ebc549ed20114
LukasAnell <lukasmanell@gmail.com>
2024-04-29 08:49:23 -0700

    Initial Commit

---
+++ app/src/main/java/com/lukasandchristine/meowmedia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lukasandchristine.meowmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
