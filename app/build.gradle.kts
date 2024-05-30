ead9ed2b22080c9c19651bf838a94156de3abb6d
LukasAnell <lukasmanell@gmail.com>
2024-05-29 18:33:07 -0700

    Updated gradle stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
@@ -55,7 +55,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.squareup.picasso:picasso:2.8")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation(group = "com.backendless", name = "backendless", version = "6.3.6")
     implementation("io.socket:socket.io-client:1.0.0") {
